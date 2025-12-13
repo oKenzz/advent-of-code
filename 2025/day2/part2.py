9a563f437aa4d3621090fb4be0c31073b08e3de7
oKenzz <kenz.phu@gmail.com>
2025-12-13 20:48:34 +0100

    day2 puzzle 2 done

---
+++ 2025/day2/part2.py
@@ -0,0 +1,45 @@
+def main():
+    with open("input.txt", "r") as file:
+        input = file.read().split(",")
+        count = 0
+        for id_range in input:
+            ranges = id_range.split("-")
+            interval = int(ranges[1]) - int(ranges[0])
+            current = int(ranges[0])
+            for i in range(interval + 1):
+                combinations = find_unique_substrings(str(current))
+                valid = check_if_id_valid(str(current), combinations)
+                if not valid:
+                    count += current
+                print(f"Id: {current}, valid: {valid}")
+                current += 1
+    print(count)
+
+
+def check_if_id_valid(id: str, substrings: set) -> bool:
+    length = len(id)
+    for substring in substrings:
+        current = ""
+        for i in range(length):
+            if substring == id:
+                continue
+            current += substring
+            if (current == id) and (len(current) == length):
+                return False
+    return True
+
+
+def find_unique_substrings(string: str) -> set:
+    length = len(string)
+    unique_substrings = set()
+    for i in range(length):
+        current = ""
+        for j in range(i, length):
+            current += string[j]
+            unique_substrings.add(current)
+
+    return unique_substrings
+
+
+if __name__ == "__main__":
+    main()
